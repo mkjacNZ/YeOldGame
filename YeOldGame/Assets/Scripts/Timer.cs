27afb1757b49c0626bc3a6669e226e577ac66cec
Mel <mkjacobsonnz+port@gmail.com>
2023-02-17 16:50:58 +1300

    .

--- YeOldGame/Assets/Scripts/Timer.cs
+++ YeOldGame/Assets/Scripts/Timer.cs
@@ -22,7 +22,9 @@ public class Timer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        gameOver = false;
         startTime = Time.time;
+        endTime = 0f;
     }
 
     // Update is called once per frame
